5bc9046047ea4b195d48e7a7d7ab117aad71939d
mateusw12 <38771418+mateusw12@users.noreply.github.com>
2023-07-24 18:53:13 -0300

    Remove arquivos de tsconfig nao utilizado

--- Pokedex/vite.config.ts
+++ Pokedex/vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import tsConfigPaths from "vite-plugin-ts";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsConfigPaths()],
+  plugins: [react()],
 });
