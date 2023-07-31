348bf36234a9540d40e3ee6f2a79eb385e305e3c
mateusw12 <38771418+mateusw12@users.noreply.github.com>
2023-07-31 20:20:37 -0300

    Ajusta path absoluto das imagens dos pokemons gigantamax

--- Pokedex/src/constant/poke-gigantamax.ts
+++ Pokedex/src/constant/poke-gigantamax.ts
@@ -1,183 +1,180 @@
 import { MegaEvolution } from "../interface/mega-evolution";
 
+const absolutePath = "/public/assets/gigantamax/";
+
 export const POKE_GIGANTAMAX: MegaEvolution[] = [
   {
-    img: "/public/assets/gigantamax/venossauro.png",
+    img: `${absolutePath}venossauro.png`,
     name: "Venusaur",
     types: ["grass", "poison"],
   },
   {
-    img: "/public/assets/gigantamax/charizard.png",
+    img: `${absolutePath}charizard.png`,
     name: "Charizard",
     types: ["fire", "flying"],
   },
   {
-    img: "/public/assets/gigantamax/blastoise.png",
+    img: `${absolutePath}blastoise.png`,
     name: "Blastoise",
     types: ["water"],
   },
   {
-    img: "/public/assets/gigantamax/buterfree.png",
+    img: `${absolutePath}buterfree.png`,
     name: "Butterfree",
     types: ["bug", "flying"],
   },
   {
-    img: "/public/assets/gigantamax/pikachu.png",
+    img: `${absolutePath}pikachu.png`,
     name: "Pikachu",
     types: ["eletric"],
   },
   {
-    img: "/public/assets/gigantamax/meowth.png",
+    img: `${absolutePath}meowth.png`,
     name: "Meowth",
     types: ["normal"],
   },
   {
-    img: "/public/assets/gigantamax/machammp.png",
+    img: `${absolutePath}machammp.png`,
     name: "Machamp",
     types: ["fighting"],
   },
   {
-    img: "/public/assets/gigantamax/gengat.png",
+    img: `${absolutePath}gengat.png`,
     name: "Gengar",
     types: ["ghost", "poison"],
   },
   {
-    img: "/public/assets/gigantamax/kingler.png",
+    img: `${absolutePath}kingler.png`,
     name: "Kingler",
     types: ["water"],
   },
   {
-    img: "/public/assets/gigantamax/lapras.png",
+    img: `${absolutePath}lapras.png`,
     name: "Lapras",
     types: ["ice","water"],
   },
   {
-    img: "/public/assets/gigantamax/snorlax.png",
+    img: `${absolutePath}eevee.png`,
     name: "Eevee",
     types: ["normal"],
   },
   {
-    img: "/public/assets/gigantamax/snorlax.png",
+    img: `${absolutePath}snorlax.png`,
     name: "Snorlax",
     types: ["normal"],
   },
-
   {
-    img: "/public/assets/gigantamax/garbodor.png",
+    img: `${absolutePath}garbodor.png`,
     name: "Garbodor",
     types: ["poison"],
   },
   {
-    img: "/public/assets/gigantamax/melmetal.png",
+    img: `${absolutePath}melmetal.png`,
     name: "Melmetal",
     types: ["water"],
   },
-
   {
-    img: "/public/assets/gigantamax/Rillaboom.png",
+    img: `${absolutePath}Rillaboom.png`,
     name: "Rillaboom",
     types: ["grass"],
   },
   {
-    img: "/public/assets/gigantamax/Cinderace.png",
+    img: `${absolutePath}Cinderace.png`,
     name: "Cinderace",
     types: ["fire"],
   },
   {
-    img: "/public/assets/gigantamax/Inteleon.png",
+    img: `${absolutePath}Inteleon.png`,
     name: "Inteleon",
     types: ["water"],
   },
   {
-    img: "/public/assets/gigantamax/Corviknight.png",
+    img: `${absolutePath}Corviknight.png`,
     name: "Corviknight",
     types: ["flying", "steel"],
   },
   {
-    img: "/public/assets/gigantamax/Orbeetle.png",
+    img: `${absolutePath}Orbeetle.png`,
     name: "Orbeetle",
     types: ["bug", "psychic"],
   },
   {
-    img: "/public/assets/gigantamax/Drednaw.png",
+    img: `${absolutePath}Drednaw.png`,
     name: "Drednaw",
     types: ["water", "rock"],
   },
   {
-    img: "/public/assets/gigantamax/Coalossal.png",
+    img: `${absolutePath}Coalossal.png`,
     name: "Coalossal",
     types: ["rock", "fire"],
   },
-
   {
-    img: "/public/assets/gigantamax/Flapple.png",
+    img: `${absolutePath}Flapple.png`,
     name: "Flapple",
     types: ["grass", "dragon"],
   },
   {
-    img: "/public/assets/gigantamax/Appletun.png",
+    img: `${absolutePath}Appletun.png`,
     name: "Appletun",
     types: ["grass", "dragon"],
   },
-
   {
-    img: "/public/assets/gigantamax/Sandaconda.png",
+    img: `${absolutePath}Sandaconda.png`,
     name: "Sandaconda",
     types: ["ground"],
   },
   {
-    img: "/public/assets/gigantamax/Toxtricity.png",
+    img: `${absolutePath}Toxtricity.png`,
     name: "Toxtricity",
     types: ["eletric", "poison"],
   },
   {
-    img: "/public/assets/gigantamax/Centiskorch.png",
+    img: `${absolutePath}Centiskorch.png`,
     name: "Centiskorch",
     types: ["fire", "bug"],
   },
-
   {
-    img: "/public/assets/gigantamax/Hatterene.webp",
+    img: `${absolutePath}Hatterene.webp`,
     name: "Hatterene",
     types: ["psychic", "fairy"],
   },
   {
-    img: "/public/assets/gigantamax/Grimmsnarl.png",
+    img: `${absolutePath}Grimmsnarl.png`,
     name: "Grimmsnarl",
     types: ["darl", "fairy"],
   },
   {
-    img: "/public/assets/gigantamax/Alcremie.png",
+    img: `${absolutePath}Alcremie.png`,
     name: "Alcremie",
     types: ["fairy"],
   },
   {
-    img: "/public/assets/gigantamax/Copperajah.png",
+    img: `${absolutePath}Copperajah.png`,
     name: "Copperajah",
     types: ["steel"],
   },
   {
-    img: "/public/assets/gigantamax/Duraludon.png",
+    img: `${absolutePath}Duraludon.png`,
     name: "Duraludon",
     types: ["steel", "dragon"],
   },
   {
-    img: "/public/assets/gigantamax/Urshifu Single.png",
+    img: `${absolutePath}Urshifu Single.png`,
     name: "Urshifu Single",
     types: ["fighting", "dark"],
   },
   {
-    img: "/public/assets/gigantamax/Urshifu Rapid.png",
+    img: `${absolutePath}Urshifu Rapid.png`,
     name: "Urshifu Rapid",
     types: ["fighting", "water"],
   },
   {
-    img: "/public/assets/gigantamax/Eternatus.png",
+    img: `${absolutePath}Eternatus.png`,
     name: "Eternatus",
     types: ["poison", "dragon"],
   },
   {
-    img: "/public/assets/gigantamax/Calyrex.png",
+    img: `${absolutePath}Calyrex.png`,
     name: "Calyrex",
     types: ["psychic", "grass"],
   },
